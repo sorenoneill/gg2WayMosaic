10e836082f2af0c57b25ed10fa999bb0eff68d04
Søren O'Neill <soren@oneill.dk>
2016-07-16 11:40:22 +0200

    Added geom_text to plot which displays raw data values inside each rectangle

--- gg2WayMosaic.R
+++ gg2WayMosaic.R
@@ -1,17 +1,24 @@
-gg2WayMosaic <- function(dfInput, air=0) {
+gg2WayMosaic <- function(dfInput, air=0, colTextAir=0, displayRawData=TRUE, displayRawDataMinimum = 0, displayRawDataSize=2) {
   # This function takes as input a data frame with two columns
   # Each column should be of class 'factor' and it is probably wise that they should be 'ordered'
   # By default, the first column will be represented along the x-axis and the second along the y-axis
+  
+  # The variable 'air' (numeric) provides a bit of space around each rectangle
+  # The variable 'colTextAir ' (numeric) provides a bit space between the columns and column text (if displayRawData = T)
+  # The variable 'displayRawData' (TRUE/FALSE) determines whether to print data values on the plot
+  # The variable 'displayRawDataMinimum' (numeric) sets a lower cut-off point for printing raw data in rectangles
+  
   # The function only produces 2-way mosaic plots (i.e. not n-way mosaic plots).
   # The function returns a ggplot object, which means the plot can be manipulated further as per usual
-  # Read more about Mosiac plots at e.g. http://www.math.yorku.ca/SCS/sugi/sugi17-paper.html
+  # Read more about Mosiac plots at e.g. http://www.math.yorku.ca/SCS/sugi/sugi17-paper.html or in the
+  # book 'Visualizing Categorical Data' by Michael Friendly
   
   # Example:
   # testData <- data.frame(
   #                   x = sample(c("A", "B", "C", "C", "C"), 200, replace=TRUE), 
   #                   y = sample(c("i", "j", "k", "l", "m"), 200, replace=TRUE)
   #             )
-  # gg2WayMosaic(testData, air=0.005)
+  #  gg2WayMosaic(testData, air=0.005, colTextAir=0.02, displayRawData=TRUE, displayRawDataMinimum = 0, displayRawDataSize=3)
   
   require(ggplot2)
   require(dplyr)
@@ -30,7 +37,6 @@ gg2WayMosaic <- function(dfInput, air=0) {
   # First calculate the absolute frequency of each combination of the x and y categorical variables
   # This is similar to the 'table' function, but note that 'count' produces a long data frame with new column 'n'
   dfResult <- count(dfInput, x, y) 
-  
   
   # Now calculate the width (w) and position (xmin , xmax) of each column (x values), as:
   # width = the relative frequency of each x-value .. i.e. if there are 100 observations and 20 of those 
@@ -58,6 +64,25 @@ gg2WayMosaic <- function(dfInput, air=0) {
     geom_rect(data=dfResult, mapping=aes(xmin=xmin+air, xmax=xmax-air, ymin=ymin+air, ymax=ymax-air, fill=factor(y)), color="black", alpha=1) +
     scale_x_continuous(limits=c(0,1), breaks=xBreaks$breaks, labels = xBreaks$x) +
     labs(title="Mosaic", x="Var x", y="Relative frequency", fill="Var y")
+  
+  if(displayRawData) {
+    
+    colText <- dfResult %>% group_by(x) %>% summarize(n=sum(n)) %>% left_join(., xBreaks)
+    dfSumY <- dfResult %>% group_by(y) %>% summarize(n=sum(n))
+    
+    # Plot the raw data inside every rectangle (if it is greater than the set minimum)
+    plot <- plot +
+      geom_text(data=dfResult, aes(x=xmin+0.5*w, y=ymin+0.5*h, label=ifelse(n<displayRawDataMinimum, "", n)), size=displayRawDataSize)
+    
+    # Plot the column sum (of raw data) above each column
+    plot <- plot +
+      geom_text(data = colText, aes(x=breaks, y=1+colTextAir, label=n), size=displayRawDataSize)
+    
+    # Reverse the order of elements in the legend
+    plot <- plot +
+      scale_fill_discrete(guide = guide_legend(reverse=TRUE))
+    
+  }
   
   return(plot)
 }
